cd9c764807e3e00e9038b1c3abb30e099b71aa78
abhishek <abbhishekk27@gmail.com>
2023-03-30 00:45:37 +0530

    success

--- index.ts
+++ index.ts
@@ -3,7 +3,34 @@ import { supabase } from './src/supabase';
 
 async function main() {
 	const data = await getData();
-	console.log(data);
+
+	if (!data || data.length === 0) {
+		console.log('no data found');
+		return;
+	}
+
+	const parsedData: object[] = data.map((row) => {
+		return {
+			name: row[0],
+			tagline: row[1],
+			description: row[2],
+			categories: row[6].split(',').map((category: string) => category.trim()),
+			status: row[7],
+			website: row[8],
+			twitter: row[9],
+			discord: row[10],
+			telegram: row[11],
+		};
+	});
+
+	const { error } = await supabase.from('projects').insert(parsedData);
+
+	if (error) {
+		console.error(error);
+		return;
+	}
+
+	console.log('done');
 }
 
 main();
--- src/gsheets.ts
+++ src/gsheets.ts
@@ -3,23 +3,19 @@ import { google } from 'googleapis';
 
 dotenv.config();
 
+const SHEET_ID = process.env.SHEET_ID!;
+
 export async function getData() {
-	const SHEET_ID = process.env.SHEET_ID!;
 	const auth = await google.auth.getClient({
 		scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
 	});
+
 	const sheets = google.sheets({ version: 'v4', auth });
 
 	const response = await sheets.spreadsheets.values.get({
 		spreadsheetId: SHEET_ID,
-		range: 'Sheet1!A2:M2',
+		range: 'Sheet1!A2:M71',
 	});
 
-	const data = response.data.values;
-	if (!data || data.length === 0) {
-		console.log('no data found');
-		return [];
-	}
-
-	return data;
+	return response.data.values;
 }
